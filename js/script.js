71cb6910f4c46793c910a68a8bc6050914970206
VadimKorobec <_s_e_t__@ukr.net>
2024-03-07 14:45:45 +0200

    add code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+let num = 50;
 
+while (num < 55) {
+  console.log(num);
+  num += 1;
+}
+
+do {
+  console.log(num);
+  num += 1;
+} while (num < 60);
+
+for (let i = 0; i <= 10; i += 1) {
+  if (i % 3) {
+    continue;
+  }
+  console.log(i);
+}
