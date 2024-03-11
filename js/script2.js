2602b66897218eba9042fc0d85abdb3f45fb22c3
VadimKorobec <_s_e_t__@ukr.net>
2024-03-11 18:37:41 +0200

    add code

---
+++ js/script2.js
@@ -0,0 +1,21 @@
+const students = [
+  "Peter",
+  "Andrew",
+  "Ann",
+  "Mark",
+  "Josh",
+  "Sandra",
+  "Cris",
+  "Bernard",
+  "Takesi",
+  "Sam",
+];
+
+function sortStudentsByGroups(arr) {
+  const newGroup = arr.sort();
+    console.log(newGroup);
+    
+    new
+}
+
+sortStudentsByGroups(students);
