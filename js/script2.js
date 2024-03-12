a56c3380e4e5e1667bb0692bea164630fccd4f26
VadimKorobec <_s_e_t__@ukr.net>
2024-03-12 14:08:58 +0200

    add some code

--- js/script2.js
+++ js/script2.js
@@ -1,21 +1,47 @@
-const students = [
-  "Peter",
-  "Andrew",
-  "Ann",
-  "Mark",
-  "Josh",
-  "Sandra",
-  "Cris",
-  "Bernard",
-  "Takesi",
-  "Sam",
-];
+"use strict";
 
-function sortStudentsByGroups(arr) {
-  const newGroup = arr.sort();
-    console.log(newGroup);
-    
-    new
-}
+const playlist = {
+  name: "My playlist",
+  rating: 5,
+  tracks: ["track-1", "track-2", "track-3", "track-4"],
+  trackCount: 4,
+  changeName(newName) {
+    this.name = newName;
+  },
+  addTrack(newTrack) {
+    this.tracks.push(newTrack);
+  },
+  updateRating(newRating) {
+    this.rating = newRating;
+  },
+  getTrackCount() {
+    return this.tracks.length;
+  },
+};
 
-sortStudentsByGroups(students);
+playlist.changeName("Mango");
+playlist.addTrack("track-5");
+playlist.updateRating(10);
+
+console.log(playlist);
+
+// const username = "Mango";
+// const email = "mango@mail.com";
+
+// const signupData = {
+//   username,
+//   email,
+// };
+
+// console.log(signupData);
+
+// const inputName = "color";
+// const inputValue = "tomato";
+
+// const colorPickerData = {
+//   [inputName]: inputValue,
+// };
+
+// console.log(colorPickerData);
+
+// console.log([]);
