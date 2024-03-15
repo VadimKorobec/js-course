9aa75393f6c0a2c996e30169aeaf6771c09ec789
VadimKorobec <_s_e_t__@ukr.net>
2024-03-15 16:48:43 +0200

    add some code

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-const btn = document.querySelector("button");
-const overlay = document.querySelector(".overlay");
-const link = document.querySelector("a");
+// const btn = document.querySelector("button");
+// const overlay = document.querySelector(".overlay");
+// const link = document.querySelector("a");
 
-let i = 0;
+// let i = 0;
 
-const deleteBtn = (e) => {
-  console.log(e.target);
-  i += 1;
-  if (i === 1) {
-    btn.removeEventListener("click", deleteBtn);
+// const deleteBtn = (e) => {
+//   console.log(e.target);
+//   i += 1;
+//   if (i === 1) {
+//     btn.removeEventListener("click", deleteBtn);
+//   }
+// };
+
+// btn.addEventListener("click", deleteBtn);
+
+// const clickLink = (e) => {
+//   e.preventDefault();
+//   console.log("click link");
+// };
+
+// link.addEventListener("click", clickLink);
+
+// console.log(document.body);
+// console.log(document.body.firstElementChild);
+// // console.log(document.body.lastChild);
+
+// console.log(document.querySelector("#current").parentElement.parentElement);
+// console.log(document.querySelector("[data-current='3']").nextElementSibling);
+
+for (const node of document.body.childNodes) {
+  if (node.nodeName === "#text") {
+    continue;
   }
-};
-
-btn.addEventListener("click", deleteBtn);
-
-const clickLink = (e) => {
-  e.preventDefault();
-  console.log("click link");
-};
-
-link.addEventListener("click", clickLink);
+  console.log(node);
+}
