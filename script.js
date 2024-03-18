100d933f94fca5ae25279360939b2e7a7cb8229e
VadimKorobec <_s_e_t__@ukr.net>
2024-03-18 16:29:29 +0200

    add some code

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-'use strict';
+"use strict";
 
-const box = document.querySelector('.box');
-const block = document.querySelector('.block');
+// const boxesQuery = document.querySelectorAll(".box");
+// const boxesGet = document.getElementsByClassName("box");
 
-console.log(block)
+// boxesQuery.forEach((item) => console.log(item.matches(".this")));
+
+// // console.log(boxesGet);
+// // console.log(boxesQuery);
+
+// // boxesQuery.forEach((item) => console.log(item));
+
+// // for (let i = 0; i < boxesGet.length; i += 1) {
+// //   console.log(boxesGet[i]);
+// // }
+
+// const obj = {
+//   name: "Tast",
+//   [Symbol("id")]: 1,
+// };
+
+// // let id = Symbol("id");
+// // obj[id] = 1;
+
+// console.log(obj);
+
+// for (const value in obj) {
+//   console.log(value);
+// }
+
+// const myAwesomeDB = {
+//   movies: [],
+//   actors: [],
+//   id: 123,
+// };
+
+const user = {
+  name: "Alex",
+  surname: "Smith",
+  birthday: "20/04/1993",
+  showMyPublicData() {
+    console.log(`${this.name} ${this.surname}`);
+  },
+};
