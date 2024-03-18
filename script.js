1dbfd3985ecc88015b7b807a702f25afce69383b
VadimKorobec <_s_e_t__@ukr.net>
2024-03-18 14:43:54 +0200

    add some code

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
-"use strict";
+'use strict';
 
-const box = document.querySelector(".box");
+const box = document.querySelector('.box');
+const block = document.querySelector('.block');
 
-const newHeight = 110;
-const newWidth = 400;
-
-function changeParams(elem, h, w) {
-  elem.style.height = `${h || 200}px`;
-  elem.style.width = `${w || 200}px`;
-  elem.innerHTML = (h ?? 200) * (w ?? 200);
-}
-
-changeParams(box, newHeight, newWidth);
-
-let userName;
-let userKey;
-
-console.log((userName && userKey) ?? "User");
+console.log(block)
